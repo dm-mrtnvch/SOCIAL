4ce90c0664034ca0be476a891b35bf301c583afe
DM MRTNVCH <69173041+dm-mrtnvch@users.noreply.github.com>
2020-12-07 22:14:47 +0300

    social-network
    11

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Technologies from './Technologies';
+import Header from './Header';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <div>
+            <a href={'#s'}>Home </a>
+            <a href={'#s'}>News </a>
+            <a href={'#s'}>Messages </a>
+        </div>
+    )
+}
+
+
+export default Header;
