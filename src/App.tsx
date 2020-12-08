942dd7708963a131aade9545391023650a67a38b
DM-MRTNVCH <dmvshn@gmail.com>
2020-12-09 01:45:16 +0300

    creating new components:
    Header.tsx
    Navbar.tsx
    Profile.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import './App.css';
-import Technologies from './Technologies';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 function App() {
     return (
-        <div>
-            <Footer />
+        <div className={'app-wrapper'}>
             <Header />
-            <Technologies />
-            <Footer />
-            <Header />
-            <Technologies />
-            <Footer />
-            <Header />
-            <Technologies />
+            <Navbar />
+            <Profile />
         </div>
     );
 }
-
-
-
-
-
-
 
 
 export default App;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+function Profile() {
+    return (
+        <div className={'content'}>
+            <div>
+                <img
+                    src="https://www.vespa.com/mediaObject/vespa/travel/travel-nha-trang/02-Opening-Image/Opening-Image/original/Opening-Image.jpg"
+                    alt="sea"/>
+            </div>
+            <div>
+                ava + descr
+            </div>
+            <div>
+                my posts
+                <div>new post</div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
