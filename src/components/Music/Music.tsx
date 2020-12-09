640354e1e751174bde2f782cb80c08c8d515123c
DM-MRTNVCH <dmvshn@gmail.com>
2020-12-10 01:32:53 +0300

    BrowserRouter Route new components:    News.tsx    Music.tsx    Settings.tsx

---
+++ src/components/Music/Music.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Music() {
+    return (
+        <div>
+            Music
+        </div>
+    )
+}
+
+export default Music;
---
+++ src/components/News/News.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Music.module.css';
+
+
+
+function News() {
+    return (
+        <div>
+            News
+        </div>
+    )
+}
+
+export default News;
