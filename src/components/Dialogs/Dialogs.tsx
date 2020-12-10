016772f6eb6a493d73852da6f826a0caec700374
DM-MRTNVCH <dmvshn@gmail.com>
2020-12-10 17:44:48 +0300

    Dialogs.tsx modification
    display: grid / grid-template-columns

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -5,7 +5,21 @@ import s from './Dialogs.module.css'
 function Dialogs() {
     return (
         <div>
-            Dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogItems}>
+                    <div className={`${s.dialog} ${s.active}`}>dymich</div>
+                    <div className={s.dialog}>sveta</div>
+                    <div className={s.dialog}>viktor</div>
+                    <div className={s.dialog}>valera</div>
+                    <div className={s.dialog}>anrey</div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>raz</div>
+                    <div className={s.message}>dva</div>
+                    <div className={s.message}>tri</div>
+                </div>
+
+            </div>
         </div>
     )
 }
